bca0cb4f13e0ffe97d64832f3191dbfe5e85ae11
KentaroKoga <kkenn1210@gmail.com>
2018-06-25 12:03:09 +0900

    Changed some text

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,9 +3,9 @@
 
 <div class="content">
     <div class="guide" v-if="!active">
-      <h1>思いつき英語アプリ</h1>
-      <h2>発音と文法の練習が一気にできます。多分ww</h2>
-      <p>このページをChromeで開いてマイクの使用を許可してください。</p>
+      <h1>思いつき英語発音文法アプリ</h1>
+      <h2>発音と文法の練習が一気にできます。</h2>
+      <p>＊このページをChrome(PC)で開いてマイクの使用を許可してください。</p>
       <p>「Start」ボタンを押して英語を話してみてください。</p>
       <p>文法が間違えていたら自動的にハイライトしてくれます。</p>
       <p>Please open this page in Chrome(PC) and allow to use a microphone.</p>
@@ -14,19 +14,19 @@
     </div>
 
     <div class="encourage" v-if="active">
-      <h2>難しいけど練習にはなるかと思いマッスルボディー</h2>
-      <p>頑張って早めに正確に喋ってください笑</p>
+      <h2>難しいけど練習にはなるかと思います。</h2>
+      <p>頑張って速めに正確に喋ってください笑</p>
       <p>発音と文法の練習です！！</p>
       <p>また、最初からしたい場合はページをリロードしてください。。。</p>
-      <p>本当にごめんなさい。なおしますので。。ご勘弁を。。。(やり方よくわからないので誰かヘルプミーww)</p>
+      <p>本当にごめんなさい。なおしますので。。ご勘弁を。。。(やり方よくわからないので誰かヘルプww)</p>
       Twitter : <a href="https://twitter.com/kentaro_koga">@kentaro_koga</a>
-      <p>Please speak fast and properly so this stupid app will understand your English.</p>
+      <p>Please speak fast and clearly so this app will understand your English.</p>
       <p>Reload this page to restart.(I'm sorry. I'll work on this issue.lol)</p>
       <p>Enjoy!</p>
     </div>
     <button class="startbtn" v-if="!active" v-on:click="toggle">Start speaking English</button>
     <button class="stopbtn" v-if="active" v-on:click="reload">Reload this page</button>
-    <h3 v-if="active">↓↓↓下に喋った英語が表示されますよ〜〜〜↓↓↓</h3>
+    <h3 v-if="active">↓↓↓下に喋った英語が表示されます↓↓↓</h3>
     <!-- <button class="stopbtn" v-if="active" v-on:click="toggle">Stop</button> -->
   </div>
 
